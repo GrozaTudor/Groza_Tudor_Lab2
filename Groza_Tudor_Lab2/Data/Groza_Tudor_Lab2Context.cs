31521a4a53b80c49123ebed4bfd1548f8b0d2b28
Groza Tudor <tudorgroza1969@yahoo.com>
2023-10-22 17:30:35 +0300

    Add project files.

---
+++ Groza_Tudor_Lab2/Data/Groza_Tudor_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Groza_Tudor_Lab2.Models;
+
+namespace Groza_Tudor_Lab2.Data
+{
+    public class Groza_Tudor_Lab2Context : DbContext
+    {
+        public Groza_Tudor_Lab2Context (DbContextOptions<Groza_Tudor_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Groza_Tudor_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Groza_Tudor_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
