b34e66a130242ebe6709f3c15c44dc8a5baa785a
Groza Tudor <tudorgroza1969@yahoo.com>
2023-11-16 19:33:08 +0200

    lab5

--- Groza_Tudor_Lab2/Models/Book.cs
+++ Groza_Tudor_Lab2/Models/Book.cs
@@ -11,6 +11,7 @@ namespace Groza_Tudor_Lab2.Models
         public string? Title { get; set; }
 
         [Column(TypeName = "decimal(6,2)")]
+        [Range(0.01, 500)]
         public decimal Price { get; set; }
         [DataType(DataType.Date)]
         public DateTime PublishingDate { get; set; }
--- Groza_Tudor_Lab2/Pages/Books/Edit.cshtml.cs
+++ Groza_Tudor_Lab2/Pages/Books/Edit.cshtml.cs
@@ -8,9 +8,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Groza_Tudor_Lab2.Data;
 using Groza_Tudor_Lab2.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Groza_Tudor_Lab2.Pages.Books
 {
+    [Authorize(Roles = "Admin")]
     public class EditModel : BookCategoriesPageModel
     {
         private readonly Groza_Tudor_Lab2.Data.Groza_Tudor_Lab2Context _context;
